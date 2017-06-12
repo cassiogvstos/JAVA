051eb0e37762aa21f1a1729db58ce3b580b6f481
Gabriel N Silva <gabrielnsilva1@hotmail.com>
2017-06-11 23:34:32 -0300

    Iniciando metodo listar Aluno no Listegem.java

--- Listagem.java
+++ Listagem.java
@@ -17,12 +17,9 @@ public class Listagem{
         System.out.println("Numero de Registro do: "+x.getProfessor()+x.getNRegistro());
     }
 
-    public void listarpessoa(Pessoa x){
-        System.out.println("Nome do: "                  +x.getPessoa().getNome());
-        System.out.println("CPF do: "                    +x.getPessoa().getCPF());
-        System.out.println("RG do: "                          +x.getRG().getRG());
-        System.out.println("Data de Nascimento do: "+x.getPessoa().getDataNasc());
-        System.out.println("Ra do: "                 +x.getPessoa().getRaaluno());
+    public void listarAlunos(List<Alunos> alunos){
+        System.out.println("Ra : " +a.getRaaluno());
+        listarpessoa(a);
     }
 
 
