f71a266ae7e585244aeeffdb33cbaa22c51a12e5
Cassio Santos <cassiogvstos@hotmail.com>
2017-06-12 01:08:02 -0300

    menu arrumado

--- Listagem.java
+++ Listagem.java
@@ -23,48 +23,48 @@ public class Listagem{
             System.out.println("6 – Listar Turmas (todos os dados)\n");
             System.out.println("7 – Sair");
             op = teclado.nextInt(); teclado.nextLine();
+            
+                if(op == 1){
+               Professor p = new Professor();
+               String a;
+
+               System.out.println("Digite o nome do professor: ");
+               a = teclado.nextLine();
+               p.setNome(a);
+               System.out.println("Digite o CPF do professor:  ");
+               a = teclado.nextLine();
+               p.setCPF(a);
+               System.out.println("Digite o RG do professor:   ");
+               a = teclado.nextLine();
+               p.setRG(a);
+               System.out.println("Digite a data de Nascimento do professor: ");
+               a = teclado.nextLine();
+               p.setDataNasc(a);
+               System.out.println("Digite o número de registro do professor: ");
+               a = teclado.nextLine();
+               p.setNumRegistro(a);
+           }
+           else if (op ==2){
+               Aluno a = new Aluno();
+               String b;
+
+               System.out.println("Digite o nome do Aluno: ");
+               b = teclado.nextLine();
+               a.setNome(b);
+               System.out.println("Digite o CPF do Aluno:  ");
+               b = teclado.nextLine();
+               a.setCPF(b);
+               System.out.println("Digite o RG do Aluno:   ");
+               a = teclado.nextLine();
+               a.setRG(b);
+               System.out.println("Digite a data de Nascimento do Aluno: ");
+               a = teclado.nextLine();
+               a.setDataNasc(b);
+               System.out.println("Digite o número de registro do Aluno: ");
+               a = teclado.nextLine();
+               a.setraAluno(b);
+           }
         }while(op != 7);
-        
-        if(op == 1){
-            Professor p = new Professor();
-            String a;
-            
-            System.out.println("Digite o nome do professor: ");
-            a = teclado.nextLine();
-            p.setNome(a);
-            System.out.println("Digite o CPF do professor:  ");
-            a = teclado.nextLine();
-            p.setCPF(a);
-            System.out.println("Digite o RG do professor:   ");
-            a = teclado.nextLine();
-            p.setRG(a);
-            System.out.println("Digite a data de Nascimento do professor: ");
-            a = teclado.nextLine();
-            p.setDataNasc(a);
-            System.out.println("Digite o número de registro do professor: ");
-            a = teclado.nextLine();
-            p.setNumRegistro(a);
-        }
-        else if (op ==2){
-            Aluno a = new Aluno();
-            String b;
-            
-            System.out.println("Digite o nome do Aluno: ");
-            b = teclado.nextLine();
-            a.setNome(b);
-            System.out.println("Digite o CPF do Aluno:  ");
-            b = teclado.nextLine();
-            a.setCPF(b);
-            System.out.println("Digite o RG do Aluno:   ");
-            a = teclado.nextLine();
-            a.setRG(b);
-            System.out.println("Digite a data de Nascimento do Aluno: ");
-            a = teclado.nextLine();
-            a.setDataNasc(b);
-            System.out.println("Digite o número de registro do Aluno: ");
-            a = teclado.nextLine();
-            a.setNumRegistro(b);
-        }
     }      
     public void listarpessoa(Pessoa x){
         System.out.println("Nome: "              +x.getNome());
